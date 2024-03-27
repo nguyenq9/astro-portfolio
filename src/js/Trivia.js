a6409ce0dc21989d67c6ca01af1c85d50fb0062f
nguyenq9 <nguyenq9@wwu.edu>
2024-03-27 09:45:52 -0700

    congregating

--- src/js/Trivia.js
+++ src/js/Trivia.js
@@ -1,13 +1,13 @@
-function displayTrivia () {
-    fetch('https://opentdb.com/api.php?amount=1&category=9&type=multiple')
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error("Error: ", error);
-      });
-}
+// function displayTrivia () {
+//     fetch('https://opentdb.com/api.php?amount=1&category=9&type=multiple')
+//     .then((response) => response.json())
+//     .then((data) => {
+//         console.log(data);
+//       })
+//       .catch((error) => {
+//         console.error("Error: ", error);
+//       });
+// }
 
 
-export { displayTrivia }
+// export { displayTrivia }
