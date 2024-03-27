a9f115ab4d3e8e7982d4052b2cf659fd32c2510a
nguyenq9 <nguyenq9@wwu.edu>
2024-03-27 13:43:02 -0700

    graaaaah

--- src/js/Commands.js
+++ src/js/Commands.js
@@ -42,9 +42,9 @@ function displayProjects() {
 //   displayOutput("Hi my name is Thai Nguyen! 👋<br>I'm a budding web developer and Computer Science student at Western Washington University.<br>I like to build engaging websites and make cool apps using AI tools like Azure Computer Vision <br>and Vertex AI. In the past, I was a math and computer science tutor at Western, helping students<br>better understand concepts taught in their classes. I'm currently working for Western's <br>Web Communications Technology team to build web pages and web apps that will help the university.<br>When I'm not in class or at work, you can find me playing pickleball, disc golf, or trying new recipes.")
 // }
 
-function displayAbout() {
-  outputDiv.innerHTML += `<span>Hi my name is Thai Nguyen! 👋<br>I'm a budding web developer and Computer Science student at Western Washington University.<br>I like to build engaging websites and make cool apps using AI tools like Azure Computer Vision <br>and Vertex AI. In the past, I was a math and computer science tutor at Western, helping students<br>better understand concepts taught in their classes. I'm currently working for Western's <br>Web Communications Technology team to build web pages and web apps that will help the university.<br>When I'm not in class or at work, you can find me playing pickleball, disc golf, or trying new recipes.</span>`
-}
+// function displayAbout() {
+//   outputDiv.innerHTML += `<span>Hi my name is Thai Nguyen! 👋<br>I'm a budding web developer and Computer Science student at Western Washington University.<br>I like to build engaging websites and make cool apps using AI tools like Azure Computer Vision <br>and Vertex AI. In the past, I was a math and computer science tutor at Western, helping students<br>better understand concepts taught in their classes. I'm currently working for Western's <br>Web Communications Technology team to build web pages and web apps that will help the university.<br>When I'm not in class or at work, you can find me playing pickleball, disc golf, or trying new recipes.</span>`
+// }
 
 function displayBanner() {
           outputDiv.innerHTML += `
@@ -71,7 +71,11 @@ function exec(command) {
     } else if (command === 'projects') {
       displayProjects();
     } else if (command === 'about') {
-      // displayAbout();
+      displayOutput("Hi my name is Thai Nguyen.")
+    } else if (command === 'all') {
+      displayBanner();
+      displaySocial();
+      displayHelp();
     } else {
       displayOutput("Command not recognized.");
     }
